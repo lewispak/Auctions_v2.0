c74b456e42e38558d834df0985ed9c14d4127dd6
lewispak <lewisnpakoti@gmail.com>
2024-03-21 15:12:13 +1300

    Add project files.

---
+++ Auctions_v2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Auctions_v2._0.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions_v2._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Listing> Listings { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Auctions_v2.0/Models/Bid.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Auctions_v2._0.Models
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+
+        [Required]
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser? User { get; set; }
+
+        public int? ListingId { get; set; }
+        [ForeignKey("ListingId")]
+        public Listing? Listing { get; set; }
+    }
+}
