de6765d95a246275b470ed78565345bffd80e3b0
lewispak <lewisnpakoti@gmail.com>
2024-03-26 16:27:01 +1300

    Added My Bids View

--- Auctions_v2.0/Data/Services/BidsService.cs
+++ Auctions_v2.0/Data/Services/BidsService.cs
@@ -1,4 +1,5 @@
 ﻿using Auctions_v2._0.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Auctions_v2._0.Data.Services
 {
@@ -15,6 +16,12 @@ namespace Auctions_v2._0.Data.Services
         {
             _context.Bids.Add(bid);
             await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Bid> GetAll()
+        {
+            var applicationDbContext = from a in _context.Bids.Include(l => l.Listing).ThenInclude(l => l.User) select a;
+            return applicationDbContext;
         }
     }
 }
--- Auctions_v2.0/Data/Services/IBidsService.cs
+++ Auctions_v2.0/Data/Services/IBidsService.cs
@@ -5,5 +5,7 @@ namespace Auctions_v2._0.Data.Services
     public interface IBidsService
     {
         Task Add(Bid bid);
+
+        IQueryable<Bid> GetAll();
     }
 }
